e69abf7114e6d0a6010fd0d1da794bab76ee2b7a
shark9602 <93127662+shark9602@users.noreply.github.com>
2021-10-25 15:54:31 +0530

    Add files via upload

---
+++ Cookies.py
@@ -0,0 +1,10 @@
+print ("Enter the number of cookies you have:")
+cookies1 = int (input())
+cookies2 = int (input())
+cookies3 = int (input())
+if cookies1<=5 and cookies2 <=10 and cookies3 <=7:
+    print ("Need to buy more")
+elif cookies1 >=5 and cookies2 >=10 and cookies3 >=7:
+    print ("Correct")
+else:
+     print ("Not enough")
